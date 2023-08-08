3a9b2ac24c19e5dadaf6cd18012c77fddfa027dc
CosminManu <cosmin.manu2000@gmail.com>
2023-08-08 22:05:11 +0300

    added printStatement code question

---
+++ java-interview/src/PrintStatementCode.java
@@ -0,0 +1,6 @@
+public class PrintStatementCode {
+    public static void main(String[] args) {
+        System.out.println(100 + 100 + "abc");
+        System.out.println("abc" + 100 + 100);
+    }
+}
