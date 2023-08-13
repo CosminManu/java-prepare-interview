87d4a3164a2f74646ca5629c5d3f81fe69e449da
CosminManu <cosmin.manu2000@gmail.com>
2023-08-13 21:01:43 +0300

    q11 finished, q12, q13, q14

--- java-interview/src/multipleChoice/q11.java
+++ java-interview/src/multipleChoice/q11.java
@@ -14,4 +14,40 @@ public class q11 {
         this.name = name;
         System.out.println("B ");
     }
+
+    public q11(int size){
+        this.size = size;
+        System.out.println("C ");
+    }
+
+    public q11(String name, int size){
+        this();
+        this.name = name;
+        this.size = size;
+        System.out.println("D ");
+    }
+
+    public static void main(String[] args) {
+        q11 q11 = new q11("C1", 5);
+        System.out.println(q11);
+    }
+
+    public String toString() {
+        return "" + name + " " + size;
+    }
 }
+
+/*
+* What is the output?
+* a) A defaultName B C1 5
+* b) C1 5
+*  -> c) A D C1 5
+* d) A B C D
+* e) Compilation error
+* f) Exception error
+*
+* First time object 'q11' of type class q11 is built calling the ctor
+* ctor q11(String, int) is called => calls this() -> default ctor of the same class, q11()
+* -> name ='defaultName', sout('A')
+* then call back q11(string, int) and continue execution
+* */
---
+++ java-interview/src/multipleChoice/q12.java
@@ -0,0 +1,22 @@
+package multipleChoice;
+
+public class q12 {
+    public static void main(String[] args) {
+        byte a = 1;     // line 1
+        int var = a;    // line 2
+        System.out.println(var);
+    }
+}
+
+/*
+* What is the output of the following?
+*   -> a) 1
+* b) compile err line 1
+* c) compile err line 2
+* d) exception thrown at runtime
+*
+* byte - 1byte, 8bit
+* short - 2byte, 16bit
+* int - 4byte, 32bit
+* long - 8byte, 64bit
+* */
